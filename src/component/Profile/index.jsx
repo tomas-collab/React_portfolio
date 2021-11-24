548a3a2b77c2ff6992c04d6abba32507b483a81b
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-11-24 22:49:39 +0100

    profile

--- src/component/Profile/index.jsx
+++ src/component/Profile/index.jsx
@@ -1,10 +1,21 @@
 import './style.css'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
+
 
 const Profile =()=>{
+    const textRef = useRef()
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: false, 
+            strings: ['FullStack Developer', 'Video Editor', 'BSN' ] })
+    }, [])
     return(
          <div className='profile' >
-             <div>
+             <div className='d-flex'>
                 <h1 className='name'>TOMAS</h1>
+                <h3 className='about-me' ref={textRef}></h3>
              </div>
       </div>
     )
