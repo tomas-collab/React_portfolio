feea8871bef9bd68ba1626eebab08940ff491570
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-11-26 14:37:56 +0100

    profile, navbar

--- src/component/Project/index.jsx
+++ src/component/Project/index.jsx
@@ -3,7 +3,16 @@ import './style.css'
  const Projects =()=>{
     return(
          <div className='projects'>
-pfojects         </div>
+             <hr className='random-line' />
+             <ul className='projects-list'>
+                 <a href="">1</a>
+                 <a href="">2</a>
+                 <a href="">3</a>
+                 <a href="">4</a>
+                 <a href="">5</a>
+                 <a href="">6</a>
+             </ul>
+       </div>
     )
 }
 
