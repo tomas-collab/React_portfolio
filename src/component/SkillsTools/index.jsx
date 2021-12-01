c47829b8d4a2a0c5ffb6679cf2c5fe1bce59ee3b
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-12-01 19:00:19 +0100

    skills and tools

--- src/component/SkillsTools/index.jsx
+++ src/component/SkillsTools/index.jsx
@@ -5,8 +5,46 @@ const SkillsTools = () =>{
    return (
        <div className='skills-container container'>
            <div className='skill-left'>
-                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates architecto veniam, officiis ut ea, praesentium sint earum qui perspiciatis nostrum dolorem itaque! Officiis corrupti, enim reiciendis exercitationem ipsum expedita omnis.
-           </div>
+               <h1>Skills & Tools</h1>
+               <div className='skills-title'>
+                   <div>
+                       <h3>Programming Language</h3>
+                       <ul>
+                           <li>1</li>
+                           <li>2</li>
+                           <li>3</li>
+                           <li>4</li>
+                       </ul>
+                   </div>
+                   <div>
+                       <h3>Data</h3>
+                       <ul>
+                           <li>1</li>
+                           <li>2</li>
+                           <li>3</li>
+                           <li>4</li>
+                       </ul>
+                   </div>
+                   <div>
+                       <h3>Frameworks & Libraries</h3>
+                       <ul>
+                           <li>1</li>
+                           <li>2</li>
+                           <li>3</li>
+                           <li>5</li>
+                       </ul>
+                   </div>
+                   <div>
+                       <h3>Other Skills & Tools</h3>
+                       <ul>
+                           <li>1</li>
+                           <li>1</li>
+                           <li>1</li>
+                           <li>1</li>
+                       </ul>
+                   </div>
+               </div>
+            </div>
            <div className='skill-right'>
                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo excepturi eaque aliquid cumque qui perferendis eveniet sit molestias corrupti aspernatur. Voluptatum dicta dolores impedit officiis, rerum dolorem laboriosam facilis deserunt.
            </div>
