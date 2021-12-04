c303bff5e32cb6fbe08f099120af2d3f5bebd557
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-12-04 09:07:08 +0100

    contact

--- src/component/Contact/index.jsx
+++ src/component/Contact/index.jsx
@@ -1,19 +1,42 @@
 
+import { useState } from 'react'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import { IoIosArrowDropdown } from 'react-icons/io'
 import './style.css'
 const ContactMe=()=>{
+    const [Show, setShow] = useState('0')
+    const [display, setDisplay] = useState('1')
+    const [message, setMessage] = useState(false)
+    const setEmail = () =>{
+        setShow('1')
+        setDisplay('0')
+    }
     return(
          
          <div className='contact' id='contact'>
              <div className='contact-left'>
                     <h1>Get In Touch</h1>
+                    <div className='contact-left-media'>
+                        <BsLinkedin size='34'/>
+                        <BsGithub size='34' className='ml-4'/>
+                    </div>
              </div>
              <div className='contact-right'>
-                    <form className='form-submit' action="https://formsubmit.co/your@email.com" method="POST">
-                            <label htmlFor="email">Email</label>
+
+                 <h3 className='mb-2 text-white'>The best way to reach me is by email</h3>
+                     <div onClick={()=>setEmail()} style={{opacity:display}} className='display-email text-white'>
+                         click here
+                         <IoIosArrowDropdown size='24'/>
+                     </div>
+                    <form style={{opacity:Show?Show:'0'}} className='form-submit mt-5' action="https://formsubmit.co/tomasberhane14@gmail.com" method="POST">
+                            <label htmlFor="email">Your Email</label>
                             <input  id='email' placeholder='Email address' type="email" name="email" required/>
                             <label className='mt-3' htmlFor="message">Message</label>
-                            <textarea id='message' placeholder='write your message here' type="text" name="name" required/>
-                            <button className='mt-5' type="submit">Send</button>   
+                            <textarea id='message' placeholder='write your message here' type="text" name="message" required/>
+                            <button onClick={()=>setMessage(true)} className='send-message mt-5' type="submit">Send</button>   
+                            {
+                                message?<h5 className='message-reply'>Thanks,I'll reply ASAP :)</h5>: <h5>Feel free to contact me anytime</h5>
+                            }
                     </form>
              </div>
              
---
+++ src/component/Thankyou/index.jsx
@@ -0,0 +1,13 @@
+import { TiTick } from 'react-icons/ti'
+import './style.css'
+
+const Thankyou = () =>{
+    return(
+        <div>
+             <div className='tick-container'>
+               <TiTick className='tick'/>
+             </div>
+        </div>
+    )
+}
+export default Thankyou
